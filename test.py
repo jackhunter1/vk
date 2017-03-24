f7381054bcd9fe83c6fbe8e421fd693bb3901f57
Jack Hunter <jackhunter@vtomske.ru>
2017-03-24 17:01:34 +0700

    just

--- test.py
+++ test.py
@@ -10,15 +10,24 @@ else:
     import httplib as HTTP
 
 
-def user_request(id_, method_, fields):
+def user_get(id_, fields):
     conn = HTTP.HTTPSConnection("api.vk.com")
-    conn.request("GET", "/method/" + method_ + '?user_id=' + id_ + '&fields=' + fields + '&v=5.62')
+    conn.request("GET", '/method/users.get?user_id=' + id_ + '&fields=' + fields + '&v=5.62')
     resp = conn.getresponse()
     data_ = json.loads(resp.read())['response'][0]
     for i in data_:
         print(i)
-    print(data_['home_town'])
+
     return data_
 
 
-print(user_request('63028253', 'users.get', 'bdate,home_town,career,counters,crop_photo,domain'))
+def audio_get(id_, fields):
+    conn = HTTP.HTTPConnection('api.vk.com')
+    conn.request('GET', '/method/audio.get?owner_id=' + id_ + '&v=5.62')
+    resp = conn.getresponse()
+    data_ = resp.read()
+    return data_
+#print(user_get('63028253', 'bdate,home_town,career,counters,crop_photo,domain'))
+
+
+print(audio_get('63028253', ''))
